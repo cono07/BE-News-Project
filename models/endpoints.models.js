e956127267f49850bafd690a43dd542ed58519cd
cono07 <amy_oconnor07@hotmail.co.uk>
2022-02-23 19:05:13 +0000

    Condensed return and removed unnecessary .then() in endpoints model.

--- models/endpoints.models.js
+++ models/endpoints.models.js
@@ -3,14 +3,9 @@ const fs = require("fs/promises");
 exports.getEndpointsData = () => {
   let body = "";
 
-  return fs
-    .readFile("./endpoints.json", "utf-8")
-    .then((data) => {
-      body += data.toString();
-      return body;
-    })
-    .then((data) => {
-      const parsedBody = JSON.parse(data);
-      return parsedBody;
-    });
+  return fs.readFile("./endpoints.json", "utf-8").then((data) => {
+    body += data.toString();
+    const parsedBody = JSON.parse(body);
+    return parsedBody;
+  });
 };
